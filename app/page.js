ec5c114eb639f08a7767298e3a6776d524cde3f1
Umar Farooq <uf29664@gmail.com>
2024-04-11 06:23:24 -0700

    firebase

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
-import Button from '@/components/button/Button';
-import ShowListButton from '@/components/showListButton/ShowListButton';
-import ShowStudents from '@/components/showStudents/ShowStudents';
+
 import React from 'react'
+
 
 export default function page() {
  
   return (
     <div>
-    <h1 className='bg-green-500 text-3xl text-center font-bold py-5'>Student Data</h1>
-
-    <Button title='Add Student Data'/>
-    <ShowStudents/>
+ 
     </div>
   )
 }

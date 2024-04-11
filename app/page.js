1ba03a40d4ffb904457c93c873a38cdd52f5ef2b
Umar Farooq <uf29664@gmail.com>
2024-04-11 06:45:06 -0700

    firebase

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
 
 import React from 'react'
+import addStudents from '@/components/addStudents/addStudents'
 
 
 export default function page() {
- 
+
   return (
     <div>
- 
+      <div className="text-2xl font-bold text-center my-3">
+        <h1 className="">
+
+        Practice For MongoDB
+        </h1>
+      <addStudents/>
+        </div>
+
     </div>
   )
 }

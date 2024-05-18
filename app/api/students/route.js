71845e55d232e10c1d03861ee65c12027f4a4652
Umar Farooq <uf29664@gmail.com>
2024-05-17 18:06:42 -0700

    mongo

--- app/api/students/route.js
+++ app/api/students/route.js
@@ -2,16 +2,16 @@ import { NextResponse } from "next/server"
 
 var users = [
     {
-        email:'umar@gmail.com',
-        password:12344
+        email: 'umar@gmail.com',
+        password: 12344
     },
     {
-        email:'farooq@gmail.com',
-        password:1234
+        email: 'farooq@gmail.com',
+        password: 1234
     }
 ]
 
-export const GET = async ()=>{
+export const GET = async () => {
     var body = users
     return NextResponse.json(users)
 }
@@ -19,9 +19,10 @@ export const GET = async ()=>{
 
 
 
-export const POST  =async(req)=>{
-    const body =await req.json()
+export const POST = async (req) => {
+    const body = await req.json()
     body.push(users)
-   return NextResponse.json({message:'post has created'})
+    console.log(users)
+    return NextResponse.json({ message: 'post has created' })
 
 }
